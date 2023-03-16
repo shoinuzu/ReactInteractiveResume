5bfece3ccf1a31207433587473b9c58dc33d64c6
António Vieira <vieira.tono@gmail.com>
2023-03-16 14:30:47 +0000

    set up inicial react com typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MainMenu from "./modules/MainMenu";
+
+function App() {
+  return (
+    <div className="flex">
+        <MainMenu></MainMenu>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/MainMenu.tsx
@@ -0,0 +1,10 @@
+import '../index.css'
+const MainMenu = () => {
+    return (
+        <div className="fixed ">
+            some text
+        </div>
+    )
+}
+
+export default MainMenu;
